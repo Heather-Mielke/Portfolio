d5bfc6870cd959d2c6dfabf227db5fa46ddfc943
Heather Mielke <heatherlmielke@gmail.com>
2022-01-28 12:50:42 -0600

    Got my project carousel up and running

--- app.js
+++ app.js
@@ -6,4 +6,26 @@ $(() => {
     $menuButton.toggleClass('is-active');
     $mobileMenu.toggleClass('is-active');
   })
+
+  imageIndex = 0;
+  numOfImages = $('#project-images').children().length -1
+  $('.next').on('click', () => {
+    $('#project-images').children().eq(imageIndex).css('display', 'none')
+    if (imageIndex < numOfImages) {
+      imageIndex++
+    }else {
+      imageIndex = 0
+    }
+    $('#project-images').children().eq(imageIndex).css('display', 'block')
+  })
+
+  $('.back').on('click', () => {
+    $('#project-images').children().eq(imageIndex).css('display', 'none')
+    if (imageIndex > 0) {
+      imageIndex--
+    }else {
+      imageIndex = numOfImages;
+    }
+    $('#project-images').children().eq(imageIndex).css('display', 'block')
+  })
   })
